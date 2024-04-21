a3944be80617991732705ab6592b714941b319ab
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-04-21 06:44:34 -0700

    Create DBConnection.java

---
+++ JavaResources/com.mvc.util/DBConnection.java
@@ -0,0 +1,35 @@
+package com.mvc.util;
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
+public class DBConnection {
+ public static Connection createConnection()
+ {
+     Connection con = null;
+     //  String url = "jdbc:mysql://localhost:3306/kadateam";
+     String url = "jdbc:mysql://localhost:3306/customers"; //MySQL URL followed by the database name
+     String username = "root"; //MySQL username
+     String password = "admin"; //MySQL password
+     System.out.println("In DBConnection.java class ");
+      
+     try
+     {
+         try
+         {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //loading MySQL drivers. This differs for database servers 
+         } 
+         catch (ClassNotFoundException e)
+         {
+            e.printStackTrace();
+         }       
+         con = DriverManager.getConnection(url, username, password); //attempting to connect to MySQL database
+         System.out.println("Printing connection object "+con);
+     } 
+     catch (Exception e) 
+     {
+        e.printStackTrace();
+     }   
+     return con; 
+ }
+}
