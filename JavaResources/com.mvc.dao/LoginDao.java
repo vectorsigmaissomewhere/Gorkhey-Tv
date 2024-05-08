68a5c7f63e0f6c11694c8994e93f700e6ade7cd5
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-08 14:24:15 -0700

    Update LoginDao.java

--- JavaResources/com.mvc.dao/LoginDao.java
+++ JavaResources/com.mvc.dao/LoginDao.java
@@ -1,11 +1,14 @@
-package com.mvc.dao;
+ package com.mvc.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import com.mvc.bean.LoginBean;
 import com.mvc.util.DBConnection;
+
+import com.mvc.bean.PasswordEncryptionWithAes;
 
 public class LoginDao {
     public String authenticateUser(LoginBean loginBean) {
@@ -26,8 +29,12 @@ public class LoginDao {
 
             while (resultSet.next()) { // Until next row is present otherwise it return false
                 userNameDB = resultSet.getString("userName"); // fetch the values present in the database
-                passwordDB = resultSet.getString("password");
-
+                try {
+					passwordDB = PasswordEncryptionWithAes.decryptPassword(resultSet.getString("password"),"U3CdwubLD5yQbUOG92ZnHw==");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+                
                 if (userName.equals(userNameDB) && password.equals(passwordDB)) {
                     return "SUCCESS"; // If the user entered values are already present in the database, which means user has already registered so return a SUCCESS message.
                 }
@@ -44,5 +51,36 @@ public class LoginDao {
             }
         }
         return "Invalid user credentials"; // Return appropriate message in case of failure
+    }
+
+    public String getUserCity(String userName) {
+        Connection con = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        String city = null;
+        
+        try {
+            con = DBConnection.createConnection();
+            String query = "SELECT city FROM users WHERE username = ?";
+            preparedStatement = con.prepareStatement(query);
+            preparedStatement.setString(1, userName);
+            resultSet = preparedStatement.executeQuery();
+            
+            if (resultSet.next()) {
+                city = resultSet.getString("city");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (con != null) con.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+        return city;
     }
 }
