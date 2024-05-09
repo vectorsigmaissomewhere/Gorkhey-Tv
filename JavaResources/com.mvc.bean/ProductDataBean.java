973b5b8d292f361e3a91e8efbab143c224a5dc5f
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-09 10:54:20 -0700

    Create ProductDataBean.java

---
+++ JavaResources/com.mvc.bean/ProductDataBean.java
@@ -0,0 +1,59 @@
+package com.mvc.bean;
+
+public class ProductDataBean {
+    private int productId;
+    private String productName;
+    private String productDescription;
+    private String productPrice;
+    private int productStock;
+    private String productActive;
+
+    // Getters and setters
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductStock() {
+        return productStock;
+    }
+
+    public void setProductStock(int productStock) {
+        this.productStock = productStock;
+    }
+
+    public String getProductActive() {
+        return productActive;
+    }
+
+    public void setProductActive(String productActive) {
+        this.productActive = productActive;
+    }
+}
