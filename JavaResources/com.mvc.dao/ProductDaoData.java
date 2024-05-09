bb90aeafa70cd49ea5c7cb9f2adc4522ddd426d0
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-09 10:55:21 -0700

    Create ProductDaoData.java

---
+++ JavaResources/com.mvc.dao/ProductDaoData.java
@@ -0,0 +1,42 @@
+package com.mvc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.mvc.bean.ProductDataBean;
+import com.mvc.util.DBConnection;
+
+public class ProductDaoData {
+    private Connection con;
+
+    public ProductDaoData() {
+        con = DBConnection.createConnection();
+    }
+
+    public List<ProductDataBean> getAllProducts() {
+        List<ProductDataBean> products = new ArrayList<>();
+        try {
+            String query = "SELECT * FROM product";
+            PreparedStatement pstmt = con.prepareStatement(query);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                ProductDataBean product = new ProductDataBean();
+                product.setProductId(rs.getInt("productid"));
+                product.setProductName(rs.getString("name"));
+                product.setProductDescription(rs.getString("description"));
+                product.setProductPrice(rs.getString("price"));
+                product.setProductStock(rs.getInt("stock"));
+                product.setProductActive(rs.getString("active"));
+                // You can set additional properties here as needed
+                products.add(product);
+            }
+            pstmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
+}
