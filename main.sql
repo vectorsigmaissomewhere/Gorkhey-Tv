2d98287db59666358de2ce3d561e08993044cf05
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-05-10 09:28:33 -0700

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,99 +1,53 @@
-mysql> show tables;
-+---------------------+
-| Tables_in_customers |
-+---------------------+
-| cart                |
-| order_history       |
-| orderdetail         |
-| product             |
-| users               |
-+---------------------+
-5 rows in set (0.00 sec)
+create database customers;
 
-mysql> desc cart;
-+--------------+--------------+------+-----+---------+----------------+
-| Field        | Type         | Null | Key | Default | Extra          |
-+--------------+--------------+------+-----+---------+----------------+
-| cart_id      | int          | NO   | PRI | NULL    | auto_increment |
-| username     | varchar(255) | YES  |     | NULL    |                |
-| product_name | varchar(255) | YES  |     | NULL    |                |
-| quantity     | int          | YES  |     | NULL    |                |
-| price        | int          | YES  |     | NULL    |                |
-+--------------+--------------+------+-----+---------+----------------+
-5 rows in set (0.00 sec)
+CREATE TABLE Cart (
+    cart_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255),
+    product_name VARCHAR(255),
+    quantity INT,
+    price INT
+);
 
-mysql> desc order_history;
-+----------------+--------------+------+-----+---------+----------------+
-| Field          | Type         | Null | Key | Default | Extra          |
-+----------------+--------------+------+-----+---------+----------------+
-| order_id       | int          | NO   | PRI | NULL    | auto_increment |
-| username       | varchar(100) | YES  |     | NULL    |                |
-| total          | int          | YES  |     | NULL    |                |
-| payment_method | varchar(100) | YES  |     | NULL    |                |
-| transaction_id | varchar(100) | YES  |     | NULL    |                |
-| status         | tinyint(1)   | YES  |     | NULL    |                |
-| address        | varchar(255) | YES  |     | NULL    |                |
-| orderdate      | date         | YES  |     | NULL    |                |
-| deliverydate   | date         | YES  |     | NULL    |                |
-+----------------+--------------+------+-----+---------+----------------+
-9 rows in set (0.01 sec)
+CREATE TABLE Contact (
+    username VARCHAR(100),
+    messagetitle VARCHAR(255),
+    messagedescription TEXT
+);
 
-mysql> desc orderdetail;
-+----------------+--------------+------+-----+---------+----------------+
-| Field          | Type         | Null | Key | Default | Extra          |
-+----------------+--------------+------+-----+---------+----------------+
-| order_id       | int          | NO   | PRI | NULL    | auto_increment |
-| username       | varchar(100) | YES  |     | NULL    |                |
-| productName    | varchar(255) | YES  |     | NULL    |                |
-| total          | int          | YES  |     | NULL    |                |
-| payment_method | varchar(100) | YES  |     | NULL    |                |
-| transaction_id | varchar(100) | YES  |     | NULL    |                |
-| status         | tinyint(1)   | YES  |     | NULL    |                |
-| address        | varchar(255) | YES  |     | NULL    |                |
-| orderdate      | date         | YES  |     | NULL    |                |
-| deliverydate   | date         | YES  |     | NULL    |                |
-+----------------+--------------+------+-----+---------+----------------+
-10 rows in set (0.00 sec)
+CREATE TABLE OrderDetail (
+    order_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(100),
+    productName VARCHAR(255),
+    total INT,
+    payment_method VARCHAR(100),
+    transaction_id VARCHAR(100),
+    status VARCHAR(20),
+    address VARCHAR(255),
+    orderdate DATE,
+    deliverydate DATE
+);
 
-mysql> desc product;
-+-------------+--------------+------+-----+---------+----------------+
-| Field       | Type         | Null | Key | Default | Extra          |
-+-------------+--------------+------+-----+---------+----------------+
-| productid   | int          | NO   | PRI | NULL    | auto_increment |
-| name        | varchar(500) | YES  |     | NULL    |                |
-| description | varchar(500) | YES  |     | NULL    |                |
-| price       | int          | YES  |     | NULL    |                |
-| stock       | int          | YES  |     | NULL    |                |
-| active      | varchar(10)  | YES  |     | NULL    |                |
-| filename    | varchar(100) | YES  |     | NULL    |                |
-| data        | longblob     | YES  |     | NULL    |                |
-+-------------+--------------+------+-----+---------+----------------+
-8 rows in set (0.00 sec)
+CREATE TABLE Product (
+    productid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(500),
+    description VARCHAR(500),
+    price INT,
+    stock INT,
+    active VARCHAR(10),
+    filename VARCHAR(100),
+    data LONGBLOB
+);
 
-mysql> desc users;
-+--------------+--------------+------+-----+---------+----------------+
-| Field        | Type         | Null | Key | Default | Extra          |
-+--------------+--------------+------+-----+---------+----------------+
-| s1no         | int          | NO   | PRI | NULL    | auto_increment |
-| fullname     | varchar(100) | YES  |     | NULL    |                |
-| email        | varchar(100) | YES  |     | NULL    |                |
-| username     | varchar(100) | YES  |     | NULL    |                |
-| password     | varchar(100) | YES  |     | NULL    |                |
-| mobileNumber | bigint       | YES  |     | NULL    |                |
-| city         | varchar(100) | YES  |     | NULL    |                |
-| state        | varchar(100) | YES  |     | NULL    |                |
-| country      | varchar(100) | YES  |     | NULL    |                |
-| isAdmin      | varchar(100) | YES  |     | NULL    |                |
-+--------------+--------------+------+-----+---------+----------------+
-10 rows in set (0.00 sec)
+CREATE TABLE Users (
+    s1no INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fullname VARCHAR(100),
+    email VARCHAR(100),
+    username VARCHAR(100),
+    password VARCHAR(100),
+    mobileNumber BIGINT,
+    city VARCHAR(100),
+    state VARCHAR(100),
+    country VARCHAR(100),
+    isAdmin VARCHAR(100)
+);
 
-
-mysql> desc contact;
-+--------------------+--------------+------+-----+---------+-------+
-| Field              | Type         | Null | Key | Default | Extra |
-+--------------------+--------------+------+-----+---------+-------+
-| username           | varchar(100) | YES  |     | NULL    |       |
-| messagetitle       | varchar(255) | YES  |     | NULL    |       |
-| messagedescription | text         | YES  |     | NULL    |       |
-+--------------------+--------------+------+-----+---------+-------+
-3 rows in set (0.03 sec)
